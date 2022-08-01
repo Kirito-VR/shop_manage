00168f76a574c2e0e1d353966451029c01d08983
shirongfa <3187144070@qq.com>
2022-08-01 20:20:21 +0800

    商品界面制作

--- src/http/modules/good.js
+++ src/http/modules/good.js
@@ -1,9 +1,11 @@
 import {get} from "@/http/axios";
 
 
-
 // /good/getGoodList
 // 查询商品列表
 export  function getGoodList(url) {
     return get(url);
+}
+export  function addGoodList(url,params) {
+    return get(url,params);
 }
