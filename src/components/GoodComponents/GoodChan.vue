3c6ecac29c614511b825e907643078e429f08518
CQmax <67449756+Kirito-VR@users.noreply.github.com>
2022-08-02 10:30:40 +0800

    Revert "Srf"

--- src/components/GoodComponents/GoodChan.vue
+++ src/components/GoodComponents/GoodChan.vue
@@ -1,51 +1,13 @@
 <template>
-      <div class="revise" >
-        <label>编号</label><input type="text"  v-model="this.addForm.id" placeholder="商品编号不能为空">
-        <br>
-        <label>名称</label><input type="text"  v-model="this.addForm.name">
-        <br>
-        <label>价格</label><input type="text"  v-model="this.addForm.price">
-        <br>
-        <label>库存</label><input type="text" v-model="this.addForm.stock">
-        <br>
-        <label>描述</label><input type="text" v-model="this.addForm.descripe">
-        <br>
-        <label>图片</label><input type="text" v-model="this.addForm.imPath">
-        <br>
-        <button class="newAdd" @click="newAdd">确认修改</button>
-        <br>
-        <button class="newX" @click="cancel">取消</button>
-      </div>
+    
 </template>
 
 <script>
     export default {
-        name: "GoodChan",
-      data(){
-          return{
-            addForm:{
-              id:"",
-              name:"",
-              price:"",
-              stock:"",
-              descripe:"",
-              imPath:""
-            },
-          }
-      },
-      methods:{
-          cancel(){
-            this.$parent.revise();
-          }
-      }
+        name: "GoodChan"
     }
 </script>
 
 <style scoped>
-.revise{
-  width: 25%;
-  height: 200px;
-  background-color: burlywood;
-  float: left;
-}
+
 </style>
