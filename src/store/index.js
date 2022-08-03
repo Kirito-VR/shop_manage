779dc2c335fc48eca9b562913853d6f34e0eca90
soft <1548138548@qq.com>
2022-08-03 15:41:14 +0800

    首页商品数量显示功能

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,12 @@ import {createStore} from "vuex";
 import vuexAlong from "vuex-along";
 import menu from "@/store/modules/menu";
 import user from "@/store/modules/user";
-
+import good from "@/store/modules/good";
 import order from "@/store/modules/order"
 
 const store=createStore({
    modules:{
-       menu,user,order
-
-import good from "@/store/modules/good";
-const store=createStore({
-   modules:{
-       menu,user,good
+       menu,user,good,order
    },
     plugins:[vuexAlong({navTree:"navTree",tagList:"tagList",goodInf:"goodInf"})]
 });
